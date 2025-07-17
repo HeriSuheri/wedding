aed895b017d1848cc915cef6fd341af2a7fda241
Heri <herysuhery94@gmail.com>
2025-07-17 17:28:08 +0700

    fix: add footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { useState, useEffect } from "react";
 import FormGuest from "./components/FormGuest";
 
-
 function App() {
   return (
-    <div className="bg-blue-100 min-h-screen p-8">
-      <FormGuest />
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-grow">
+        <FormGuest />
+      </main>
+      <div className="bg-gray-100 h-20 flex items-center justify-center">
+        <footer className="text-center text-xs text-gray-500 py-4">
+          &copy; 2025 Heri Wedding
+        </footer>
+      </div>
     </div>
   );
 }
